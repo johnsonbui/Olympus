0db442b56b69813445a75df080d86efcbccfbf92
Johnson Bui <johnson.h.bui@gmail.com>
2019-05-29 19:48:04 -0400

    Fixed a reference to the wrong CSS file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './components/app/App.css';
+import './index.css';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 
